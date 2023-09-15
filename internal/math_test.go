ab4e816fda9b1f24dd5459690fb121055ba7df0f
André Luiz <andre.luiz@fretebras.com.br>
2023-09-15 14:03:09 -0300

    feat: add new teste

--- internal/math_test.go
+++ internal/math_test.go
@@ -8,6 +8,6 @@ func TestSoma(t *testing.T) {
 	total := Calc(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da soma é inválido: Resultado %d Esperado: %d", total, 30)
+		t.Errorf("Resultado da soma é invsálido: Resultado %d Esperado: %d", total, 30)
 	}
 }
